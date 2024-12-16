251764064650b906595d0f72bcc023d3a6e135b9
Jan Markiewicz <j.markiewicz@student.uj.edu.pl>
2024-12-16 15:25:13 +0100

    use icons for tob bar buttons

--- src/Components/TopBar/TopBarComponent.jsx
+++ src/Components/TopBar/TopBarComponent.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styles from "./TopBarStyle.module.css";
 import { Link } from "react-router-dom";
+import contrast_1_icon from "../../assets/contrast-1.svg";
+import contrast_2_icon from "../../assets/contrast-2.svg";
+import contrast_3_icon from "../../assets/contrast-3.svg";
+import font_size_1_icon from "../../assets/font-size-1.svg";
+import font_size_2_icon from "../../assets/font-size-2.svg";
+import font_size_3_icon from "../../assets/font-size-3.svg";
+import language_pl_icon from "../../assets/language-pl.svg";
 
 export default function TopBarComponent({ noStyleButtons }) {
 	return (
@@ -23,13 +30,61 @@ export default function TopBarComponent({ noStyleButtons }) {
 			</div>
 			{!noStyleButtons ? (
 				<div className={styles.topBarButtons}>
-					<button>Aa</button>
-					<button>A+</button>
-					<button>A-</button>
-					<button className={styles.highlighted}>Aa</button>
-					<button className={styles.highlighted}>Aa</button>
-					<button className={styles.highlighted}>Aa</button>
-					<button>🇵🇱</button>
+					<div className={styles.buttonGroup}>
+						<button className={styles.button}>
+							<img
+								className={styles.buttonImg}
+								src={font_size_1_icon}
+								alt="Default font size"
+							/>
+						</button>
+						<button className={styles.button}>
+							<img
+								className={styles.buttonImg}
+								src={font_size_2_icon}
+								alt="Bigger font size"
+							/>
+						</button>
+						<button className={styles.button}>
+							<img
+								className={styles.buttonImg}
+								src={font_size_3_icon}
+								alt="Biggest font size"
+							/>
+						</button>
+					</div>
+					<div className={styles.buttonGroup}>
+						<button className={styles.button}>
+							<img
+								className={styles.buttonImg}
+								src={contrast_1_icon}
+								alt="Default contrast"
+							/>
+						</button>
+						<button className={styles.button}>
+							<img
+								className={styles.buttonImg}
+								src={contrast_2_icon}
+								alt="More contrast"
+							/>
+						</button>
+						<button className={styles.button}>
+							<img
+								className={styles.buttonImg}
+								src={contrast_3_icon}
+								alt="Most contrast"
+							/>
+						</button>
+					</div>
+					<div className={styles.buttonGroup}>
+						<button className={styles.button}>
+							<img
+								className={styles.buttonImg}
+								src={language_pl_icon}
+								alt="Language - PL"
+							/>
+						</button>
+					</div>
 				</div>
 			) : null}
 		</div>
