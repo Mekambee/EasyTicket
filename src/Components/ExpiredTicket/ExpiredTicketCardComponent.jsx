2f6a0593fa2faaa96acd558359274cb02e1cefbe
Mekambee <kamil.piechotaaax@gmail.com>
2025-01-05 19:32:14 +0100

    expired component buy again changed to show ticket

--- src/Components/ExpiredTicket/ExpiredTicketCardComponent.jsx
+++ src/Components/ExpiredTicket/ExpiredTicketCardComponent.jsx
@@ -5,19 +5,23 @@ import styles from "./ExpiredTicketCardStyle.module.css";
 export default function ExpiredTicketCardComponent({ price, time }) {
   const [, navigate] = useLocation();
 
-  const handleBuyAgain = () => {
-    navigate(`/outer-site?price=${encodeURIComponent(price)}&time=${encodeURIComponent(time)}`);
+  const handleShowTicket = () => {
+    navigate(
+      `/your-ticket?price=${encodeURIComponent(price)}&time=${encodeURIComponent(
+        time
+      )}`
+    );
   };
 
   return (
     <div className={styles.ticketCard}>
       <h2>{time}</h2>
-      <p className={styles.redParagraph}>Wazność Upłyneła!</p>
+      <p className={styles.redParagraph}>Ważność wygasła!</p>
       <p>MPK Kraków</p>
       <p>strefy I+II+III</p>
       <h3>{price} zł</h3>
-      <button className={styles.activateButton} onClick={handleBuyAgain}>
-        Kup ponownie
+      <button className={styles.activateButton} onClick={handleShowTicket}>
+        Pokaż Bilet
       </button>
     </div>
   );
