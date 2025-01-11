afc35dbf33367af2223dafebfb6f03de52123924
Jan Markiewicz <j.markiewicz@student.uj.edu.pl>
2025-01-11 23:22:17 +0100

    set up style controls

--- src/Components/TopBar/TopBarComponent.jsx
+++ src/Components/TopBar/TopBarComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styles from "./TopBarStyle.module.css";
 import { Link } from "wouter";
+import { navigate } from "wouter/use-hash-location";
+
 import contrast_1_icon from "../../assets/contrast-1.svg";
 import contrast_2_icon from "../../assets/contrast-2.svg";
 import contrast_3_icon from "../../assets/contrast-3.svg";
@@ -9,46 +10,70 @@ import font_size_2_icon from "../../assets/font-size-2.svg";
 import font_size_3_icon from "../../assets/font-size-3.svg";
 import language_pl_icon from "../../assets/language-pl.svg";
 
+import styles from "./TopBarStyle.module.css";
+
 export default function TopBarComponent({ noStyleButtons }) {
-	const clearURL = () => {
-		window.history.replaceState({}, document.title, window.location.pathname + "#/");
-	};
 	return (
 		<div className={styles.topBar}>
-			<div className={styles.backHomeButton} onClick={clearURL}>
-				<Link to={"/"}>
-					<svg
-						className={styles.backHomeIcon}
-						xmlns="http://www.w3.org/2000/svg"
-						width="32"
-						height="32"
-						viewBox="0 0 24 24"
-					>
-						<path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8z" />
-					</svg>
-				</Link>
-			</div>
+			<Link
+				to="/"
+				className={styles.backHomeButton}
+				onClick={(e) => {
+					e.preventDefault();
+					navigate("/");
+				}}
+			>
+				<svg
+					className={styles.backHomeIcon}
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+				>
+					<path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8z" />
+				</svg>
+			</Link>
+
 			<div className={styles.logo}>
 				<h1>EasyTicket</h1>
 			</div>
+
 			{!noStyleButtons ? (
 				<div className={styles.topBarButtons}>
 					<div className={styles.buttonGroup}>
-						<button className={styles.button}>
+						<button
+							className={styles.button}
+							onClick={() => {
+								localStorage.setItem("font-size", "default");
+								document.documentElement.dataset.contrast = "default";
+							}}
+						>
 							<img
 								className={styles.buttonImg}
 								src={font_size_1_icon}
 								alt="Default font size"
 							/>
 						</button>
-						<button className={styles.button}>
+						<button
+							className={styles.button}
+							onClick={() => {
+								localStorage.setItem("font-size", "larger");
+								document.documentElement.dataset.contrast = "larger";
+							}}
+						>
 							<img
 								className={styles.buttonImg}
 								src={font_size_2_icon}
 								alt="Bigger font size"
 							/>
 						</button>
-						<button className={styles.button}>
+						<button
+							className={styles.button}
+							onClick={() => {
+								localStorage.setItem("font-size", "largest");
+								document.documentElement.dataset.contrast = "largest";
+							}}
+						>
 							<img
 								className={styles.buttonImg}
 								src={font_size_3_icon}
@@ -57,21 +82,39 @@ export default function TopBarComponent({ noStyleButtons }) {
 						</button>
 					</div>
 					<div className={styles.buttonGroup}>
-						<button className={styles.button}>
+						<button
+							className={styles.button}
+							onClick={() => {
+								localStorage.setItem("contrast", "default");
+								document.documentElement.dataset.contrast = "default";
+							}}
+						>
 							<img
 								className={styles.buttonImg}
 								src={contrast_1_icon}
 								alt="Default contrast"
 							/>
 						</button>
-						<button className={styles.button}>
+						<button
+							className={styles.button}
+							onClick={() => {
+								localStorage.setItem("contrast", "higher");
+								document.documentElement.dataset.contrast = "higher";
+							}}
+						>
 							<img
 								className={styles.buttonImg}
 								src={contrast_2_icon}
 								alt="More contrast"
 							/>
 						</button>
-						<button className={styles.button}>
+						<button
+							className={styles.button}
+							onClick={() => {
+								localStorage.setItem("contrast", "highest");
+								document.documentElement.dataset.contrast = "highest";
+							}}
+						>
 							<img
 								className={styles.buttonImg}
 								src={contrast_3_icon}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+document.documentElement.dataset.contrast =
+	localStorage.getItem("contrast") ?? "default";
+
+document.documentElement.dataset.fontSize =
+	localStorage.getItem("font-size") ?? "default";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
